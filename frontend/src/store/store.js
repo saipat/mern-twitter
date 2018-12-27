7ef3875984c0f543fd535d99e0fc94507c66ee4c
Sai Lakshmi Pattabiraman <sailakshmi.pattabiraman@gmail.com>
2018-12-27 08:11:59 -0800

    Finish writing signup and login

---
+++ frontend/src/store/store.js
@@ -0,0 +1,16 @@
+//applying the middleware allows us to view our state in the console window whenever we dispatch a new action to the store.
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import logger from 'redux-logger';
+
+import rootReducer from '../reducers/root_reducer';
+
+const configureStore = (preloadedState = {}) => (
+    createStore(
+        rootReducer,
+        preloadedState,
+        applyMiddleware(thunk, logger)
+    )
+);
+
+export default configureStore;
